42f7fd8d3b6ff7cee322836f04a79703ee93be05
Swastik <swastikgautam76@gmail.com>
2026-01-02 15:26:18 +0530

    this is my 2 final commit

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,11 +1,11 @@
 import os
 
 
-AWS_S3_BUCKET_NAME = "wafer-fault"
+AWS_S3_BUCKET_NAME = "waferfault1"
 MONGO_DATABASE_NAME = "pwskills"
 MONGO_COLLECTION_NAME = "waferfault"
 
-TARGET_COLUMN = "Good/Bad"
+TARGET_COLUMN = "quality"
 MONGO_DB_URL="mongodb+srv://swastikgautam76:UQgIw7LHe2t8SiYz@cluster0.obktxkg.mongodb.net/?retryWrites=true&w=majority"
 
 
